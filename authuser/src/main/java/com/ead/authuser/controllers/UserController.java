c7211b8933c64235612fd056b52092dad9a9891d
Jeferson-Leal <jeferson.gleal@outlook.com>
2024-11-18 21:52:56 -0300

    :sparkles:[feat] Implementado o endpoint deletar usuario (delete()).

--- authuser/src/main/java/com/ead/authuser/controllers/UserController.java
+++ authuser/src/main/java/com/ead/authuser/controllers/UserController.java
@@ -34,4 +34,16 @@ public class UserController {
             return ResponseEntity.status(HttpStatus.OK).body(userModelOptional.get());
         }
     }
+
+    @DeleteMapping("/{userId}")
+    public ResponseEntity<Object> deleteUser(@PathVariable(value = "userId") UUID userId) {
+        Optional<UserModel> userModelOptional = userService.findById(userId);
+
+        if (!userModelOptional.isPresent()) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("User not found!");
+        } else {
+            userService.delete(userModelOptional.get());
+            return ResponseEntity.status(HttpStatus.OK).body("User deleted success!");
+        }
+    }
 }
